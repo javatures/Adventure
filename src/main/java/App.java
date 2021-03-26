5cccfe9c775ca90e227ac24c64fa30694f82e729
Drake Taylor <drake.taylor@revature.net>
2021-03-26 15:37:52 -0500

    Rename main file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Game {
+public class App {
     public static void main (String[] args) {
         Scanner s = new Scanner(System.in);
         boolean playAgain;
