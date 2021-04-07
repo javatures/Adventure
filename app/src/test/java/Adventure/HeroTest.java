7beb8bb326de89fa5975d61e7ef4b52e7a951be2
Drake Taylor <drake.taylor@revature.net>
2021-04-06 22:22:50 -0500

    Rebuild project with gradle to allow JUnit tests

---
+++ app/src/test/java/Adventure/HeroTest.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HeroTest {
+    
+    /*@Test
+    public void constructorTest() {
+        Connection c = null;
+        try {
+            c = DriverManager.getConnection("jdbc:postgres://localhost:5432/postgres", "postgres", "password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Hero hero = new Hero(new Random(), c);
+        assertEquals(hero.getLevel(), 2);
+
+    }*/
+}
